82e4d59ab13b39274b16a30241d62fc2a5a964d4
Hamza Baloch <158695894+Hamzabaloch08@users.noreply.github.com>
2024-08-14 04:08:53 +0500

    create variables for using functions, and setintervale funcing

---
+++ app.js
@@ -0,0 +1,8 @@
+let minutes = 0
+let seconds = 0
+let miliSeconds = 0
+let getMin = document.getElementById('min')
+let getSec = document.getElementById('sec')
+let getMiliSec = document.getElementById('msec')
+let stopTheStart;
+
